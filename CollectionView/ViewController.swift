c260319aca2de2f9d48441b35610fc254c21df7d
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-03-03 16:34:53 +0300

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Dmytro Ivanenko on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    private var collectionView: UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        layout.itemSize = CGSize(width: (view.frame.size.width/3)-4,
+                                 height: (view.frame.size.width/3)-4)
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        guard let collectionView = collectionView else { return }
+        
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath) as! CustomCollectionViewCell
+        
+//        cell.contentView.backgroundColor = .systemBlue
+        cell.configure(label: "Custom, \(indexPath.row)")
+        return cell
+    }
+    
+    
+}
+
